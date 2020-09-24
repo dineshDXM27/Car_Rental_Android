24beff33c3a25e00d95f9b7d19438a96b4fd1cf5
MohammadSuraj <MohammadSuraj@users.noreply.github.com>
2020-09-23 21:40:03 -0500

    Created Empty Application Main Screen Activity
    Created Empty Application Main Screen Activity (Testing GIT HUB COMMITS)

---
+++ app/src/main/java/org/uta/rental/ApplicationMainScreen.java
@@ -0,0 +1,14 @@
+package org.uta.rental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApplicationMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_main_screen);
+    }
+}
