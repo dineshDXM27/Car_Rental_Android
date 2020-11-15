718230672fe18dc388b94c67f2e2a6929c51fc9e
raj10 <rajzp1@gmail.com>
2020-11-15 07:24:24 -0600

    Adding UpdateProfile and Subclasses

---
+++ app/src/main/java/org/uta/rental/UpdateProfileUser.java
@@ -0,0 +1,13 @@
+package org.uta.rental;
+
+public class UpdateProfileUser extends UpdateProfile{
+    private int AACMemberstatus;
+
+    public int getAACMemberstatus() {
+        return AACMemberstatus;
+    }
+
+    public void setAACMemberstatus(int AACMemberstatus) {
+        this.AACMemberstatus = AACMemberstatus;
+    }
+}
