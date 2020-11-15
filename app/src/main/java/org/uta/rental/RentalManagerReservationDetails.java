1a5f7ca2eca6270e329e0bd754c02fcc9b6c32bb
dinesh <dxm2497@mavs.uta.edu>
2020-11-14 18:09:11 -0600

    ReservationDetails and subclasses
    ReservationDetails and subclasses

---
+++ app/src/main/java/org/uta/rental/RentalManagerReservationDetails.java
@@ -0,0 +1,13 @@
+package org.uta.rental;
+
+public class RentalManagerReservationDetails extends ReservationDetails {
+    private String duration;
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
---
+++ app/src/main/java/org/uta/rental/UserReservationDetails.java
@@ -0,0 +1,14 @@
+package org.uta.rental;
+
+public class UserReservationDetails extends ReservationDetails {
+
+    private int aaMemberStatus;
+
+    public int getAaMemberStatus() {
+        return aaMemberStatus;
+    }
+
+    public void setAaMemberStatus(int aaMemberStatus) {
+        this.aaMemberStatus = aaMemberStatus;
+    }
+}
