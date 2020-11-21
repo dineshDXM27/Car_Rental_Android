841436cf02890a598d2d97a5cdac52f3654911a6
dinesh <dxm2497@mavs.uta.edu>
2020-11-20 20:10:04 -0600

    View Available Cars GUI and View Reservation Details GUI
    View Available Cars GUI and View Reservation Details GUI

--- app/src/main/java/org/uta/rental/DeleteReservationGUI.java
+++ app/src/main/java/org/uta/rental/DeleteReservationGUI.java
@@ -1,6 +1,5 @@
 package org.uta.rental;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,32 +8,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.w3c.dom.Text;
 
-public class ViewReservationDetailsGUI extends AppCompatActivity {
-    private Button logoutButton;
+public class DeleteReservationGUI extends AppCompatActivity {
+    private Button deleteButton;
     private Text reservationNumberText;
     private Text carNumber;
-    private Text carName;
     private Text carCapacity;
     private Text startDate;
     private Text startTime;
     private Text endDate;
     private Text endTime;
     private Text totalCost;
-    private Text duration;
-    private Text siriusXM;
-    private Text onStar;
+    private Text aacMemberStatus;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reservation_details_g_u_i);
+        setContentView(R.layout.activity_delete_reservation_g_u_i);
 
-        logoutButton = findViewById(R.id.logout_viewResDetails);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+        deleteButton = findViewById(R.id.delete);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ViewReservationDetailsGUI.this, ApplicationMainScreen.class));
+                showDialog();
             }
         });
+    }
+
+    public void showDialog(){
+        DeleteReservationDialog dialog = new DeleteReservationDialog();
+        dialog.show(getSupportFragmentManager(), "delete reservation dialog");
     }
 }
